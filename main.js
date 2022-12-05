3a600cc2f0c5e73723b3eec00832fa97adda837b
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-12-05 19:01:32 +0100

    Add function to show books

--- main.js
+++ main.js
@@ -13,8 +13,19 @@ const bookList = [
 const titleInput = document.querySelector('.title-book-add');
 const authorInput = document.querySelector(".author-book-add");
 const btnAdd = document.querySelector(".btn-add");
+const bookDisplay = document.querySelector(".books-display");
 // Function to add a book
 
-const addBook = function() {
+showBook.forEach((i) => {
+  const bookDetail = `
+    <div class="book-box">
+      <p class="book-position">${bookList[i].title}</p>
+      <p class="book-title">${bookList[i].author}</p>
+      <button class="remove-btn">Remove</button>
+      <div class="line-bottom"></div>
+    </div>
+  `;
+  bookDisplay.insertAdjacentHTML('beforebegin', bookDetail);
+});
 
-}
+btnAdd.addEventListener('click', addBook);
