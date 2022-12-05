494bd64d5aec07eae02836edbdb7543a2e35fd94
Andrea <andrearivas4cinfor@gmail.com>
2022-12-05 20:52:26 +0100

    Trying add book function

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 'use script'
 
-const bookList = [
-  {
-    title: "Fine balance",
-    author: "Federico Rivera",
-  },
-  {
-    title: "The mistery of the murderer",
-    author: "Sancho Perez",
-  },
-];
+
+  
+  const book = [
+    {
+      title: "Fine balance",
+      author: "Federico Rivera",
+    },
+    {
+      title: "The mistery of the murderer",
+      author: "Sancho Perez",
+    },
+  ];
+  
+
+
 const titleInput = document.querySelector('.title-book-add');
 const authorInput = document.querySelector(".author-book-add");
 const btnAdd = document.querySelector(".btn-add");
 const bookDisplay = document.querySelector(".books-display");
-// Function to add a book
 
-showBook.forEach((i) => {
+
+book.forEach((e,i) => {
   const bookDetail = `
     <div class="book-box">
-      <p class="book-position">${bookList[i].title}</p>
-      <p class="book-title">${bookList[i].author}</p>
+      <p class="book-position">${book[i].title}</p>
+      <p class="book-title">${book[i].author}</p>
       <button class="remove-btn">Remove</button>
       <div class="line-bottom"></div>
     </div>
   `;
-  bookDisplay.insertAdjacentHTML('beforebegin', bookDetail);
+  bookDisplay.insertAdjacentHTML('beforeend', bookDetail);
 });
 
+
+//Add books
+const addBook = function() {
+  const newTitle = titleInput.value;
+  const newAuthor = authorInput.value;
+  console.log(newAuthor,newTitle);
+  const Book2 = Object.assign({},book);
+  //bookDisplay.innerHTML = JSON.stringify(book);
+  Book2.title = titleInput.value;
+  Book2.author= authorInput.value;
+  
+}
 btnAdd.addEventListener('click', addBook);
