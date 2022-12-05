cb6aff0c012467b47d2c28c152da06ea38371b63
Andrea <andrearivas4cinfor@gmail.com>
2022-12-05 18:35:41 +0100

    Add html and first values at JS

---
+++ main.js
@@ -0,0 +1,20 @@
+'use script'
+
+const bookList = [
+  {
+    title: "Fine balance",
+    author: "Federico Rivera",
+  },
+  {
+    title: "The mistery of the murderer",
+    author: "Sancho Perez",
+  },
+];
+const titleInput = document.querySelector('.title-book-add');
+const authorInput = document.querySelector(".author-book-add");
+const btnAdd = document.querySelector(".btn-add");
+// Function to add a book
+
+const addBook = function() {
+
+}
