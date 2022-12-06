f4fa2be16c9a1148fee13334d68fc77394b1593e
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-12-06 01:32:08 +0100

    Create function to remove books

--- main.js
+++ main.js
@@ -1,38 +1,50 @@
 'use script'
 
-
 const titleInput = document.querySelector('.title-book-add');
 const authorInput = document.querySelector(".author-book-add");
 const btnAdd = document.querySelector(".btn-add");
+const btnRemove = document.querySelector(".btn-remove");
 const bookDisplay = document.querySelector(".books-display");
-  
+
 function Book(title, author) {
   this.title = title;
-  this.author = author
+  this.author = author;
 }
 function UI (){}
 
-
-//Add books
+// ADD BOOKS
 const addBook = function(e) {
   const title = titleInput.value;
   const author = authorInput.value;
   const book = new Book(title,author);
   const ui = new UI();
   console.log(book, ui);
-  ui.addBooks(book);
+  ui.addBook(book);
   e.preventDefault();
 }
 btnAdd.addEventListener('click', addBook);
 
-//Display books
-
-UI.prototype.addBooks = function(book) {
+UI.prototype.addBook = function(book) {
   const bookDetail = document.createElement('div');
-  bookDetail.innerHTML = 
+  bookDetail.innerHTML =
   `<p class="book-position">${book.title}</p>
   <p class="book-title">${book.author}</p>
-  <button class="remove-btn">Remove</button>
+  <button class="btn-remove">Remove</button>
   <div class="line-bottom"></div>`
   bookDisplay.appendChild(bookDetail);
+}
+
+// DELETE BOOKS
+const deleteBook = function(e) {
+  const ui = new UI();
+  console.log(e.target);
+  ui.deleteBook(e.target);
+  e.preventDefault();
+}
+bookDisplay.addEventListener('click', deleteBook);
+
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'btn-remove') {
+    target.parentElement.remove();
+  }
 }
