bf97321627c33c9707028e792728daa9b0b878f1
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-12-07 08:15:24 +0100

    Fix delete function and added local storage function

--- main.js
+++ main.js
@@ -1,62 +1,82 @@
 'use script'
 
-const newTitle = document.querySelector('#title-book-add');
-const newAuthor = document.querySelector(".author-book-add");
+// CREATE
+let book = [
+  {
+    id: 0,
+    title: 'QWERTY',
+    author: 'Henschel',
+  },
+  {
+    id: 1,
+    title: 'AZERTY',
+    author: 'Milton',
+  },
+];
+
+const titleInput = document.querySelector('.title-book-add');
+const authorInput = document.querySelector(".author-book-add");
 const btnAdd = document.querySelector(".btn-add");
 const btnRemove = document.querySelector(".btn-remove");
 const bookDisplay = document.querySelector(".books-display");
 
+// LOCAL STORAGE
+const bookValue = JSON.parse(localStorage.getItem('book'));
+if(bookValue === 0 || bookValue === null){
+  localStorage.setItem('local', JSON.stringify(book));
+} else{
+  book = bookValue;
+}
 
-const bookList = [ 
-  { 
-    title: "book 1", 
-    author: "author 1" 
-  },
- { 
-  title: "book 2", 
-  author: "author 2" } ];
-
-// Display books
-
-const displayBook = function() {
-  let html= "";
-  bookList.forEach((e,i) => {
-  html  += `
-    <div class="book-box">
-      <p class="book-position">${bookList[i].title}</p>
-      <p class="book-title">${bookList[i].author}</p>
-      <button class="remove-btn">Remove</button>
+// CREATE BOOK SECTION
+for(let i = 0; i < book.length; i++){
+  const bookInfo = `
+    <div>
+      <p class="book-position">${book[i].title}</p>
+      <p class="book-title">${book[i].author}</p>
+      <button id="${book[i].id}" class="btn-remove">Remove</button>
       <div class="line-bottom"></div>
     </div>
   `;
+  bookDisplay.innerHTML += bookInfo;
+}
+
+// ADD BOOKS
+btnAdd.addEventListener('click', (e) => {
+  e.preventDefault();
+  const newTitle = titleInput.value;
+  const newAuthor = authorInput.value;
+  let newId;
+  let len = book.length;
+  if (len === 0 || len === null) {
+    newId = 0;
+  } else {
+    newId = book[len - 1].id + 1;
+  }
+  const newBook = {
+    id: newId,
+    title: newTitle,
+    author: newAuthor,
+  }
+  book.push(newBook);
+  const bookInfo = `
+    <div>
+      <p class="book-position">${newBook.title}</p>
+      <p class="book-title">${newBook.author}</p>
+      <button id="${newBook.id}" class="btn-remove">Remove</button>
+      <div class="line-bottom"></div>
+    </div>
+  `;
+  bookDisplay.innerHTML += bookInfo;
+  localStorage.setItem('local', JSON.stringify(book)); // LOCAL STORAGE
 });
-bookDisplay.innerHTML = html;
-}
-// create book
-const createBook = function (e) {
-  e.preventDefault()
-  const newBook = {};
-  newBook.title = newTitle.value;
-  newBook.author = newAuthor.value;
-  bookList.push(newBook);
-  console.log(bookList);
-  displayBook();
-  newTitle.value = newAuthor.value = "";
-}
-btnAdd.addEventListener("click",createBook);
 
-// show books when you refresh
-window.addEventListener('load', () => {
-  displayBook();
-});
-
-//remove book
-
-const deleteBook = function(target) {
-
-}
-bookDisplay.addEventListener('click', deleteBook);
-
-document.querySelectorAll('btn-remove').forEach((btnRemove) => {
-  btnRemove.addEventListener('click', remove(btnRemove));
+// REMOVE BOOKS
+bookDisplay.addEventListener('click', (e) => {
+  if(e.target.className === 'btn-remove'){
+    const {id} = e.target;
+    book = book.filter((bk) => JSON.stringify(bk.id) !== id);
+    localStorage.setItem('local', JSON.stringify(book)); // LOCAL STORAGE
+    e.target.parentElement.remove();
+  }
 });
